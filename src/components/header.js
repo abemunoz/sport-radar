b8130617df93d23d70a526644a55085d0bd8995b
Abraham Munoz <abrahamunoz@utexas.edu>
2020-03-13 01:47:11 -0700

    starting assignment with gatsby for static pages, styled raw header

---
+++ src/components/header.js
@@ -0,0 +1,49 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `#000000`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        padding: `1.45rem 1.0875rem`,
+        display: `flex`,
+        justifyContent: `space-between`,
+        alignItems: `center`
+      }}
+    >
+      <Link to="/">
+      <img width="100px" height="100px"
+      src="//www-league.nhlstatic.com/images/logos/league-dark/133-flat.svg" alt="NHL"></img>
+      </Link>
+      <h1 class="title" style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+      <input></input>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
